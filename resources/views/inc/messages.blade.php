132c340d631edaac37198a0585b7384399ceafb3
Artisan Labs <artisan254@gmail.com>
2018-06-01 16:03:43 +0300

    Created the global alerts file. Handles all the alerts in the system.

--- resources/views/inc/messages.blade.php
+++ resources/views/inc/messages.blade.php
@@ -1,7 +1,24 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: artisan
- * Date: 6/1/18
- * Time: 2:59 PM
- */
+@if(session('error'))
+	<div class="alert alert-danger alert-dismissible">
+		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+		<h5><i class="icon fa fa-ban"></i> Error!</h5>
+		{{ session('error') }}
+	</div>
+@endif
+
+@if(session('success'))
+	<div class="alert alert-success alert-dismissible">
+		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+		<h5><i class="icon fa fa-check"></i> Success!</h5>
+		{{ session('success') }}
+	</div>
+@endif
+
+@if(session('info'))
+	<div class="alert alert-info alert-dismissible">
+		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+		<h5><i class="icon fa fa-info-circle"></i> Info!</h5>
+		{{ session('info') }}
+	</div>
+@endif
+
