0f9c6123f88c716debd3ffc7b5830fb08e08b1fa
Artisan Labs <artisan254@gmail.com>
2018-06-04 16:18:13 +0300

    Changed the channel to database. Added the title and the message to be stored.

--- app/Notifications/AccountActivity.php
+++ app/Notifications/AccountActivity.php
@@ -3,59 +3,59 @@
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Notification;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
 
-class AccountActivity extends Notification
-{
-    use Queueable;
+class AccountActivity extends Notification {
+	use Queueable;
+	private $title;
+	private $message;
 
-    /**
-     * Create a new notification instance.
-     *
-     * @return void
-     */
-    public function __construct(string $title, string $message)
-    {
-        //
-    }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
-    public function via($notifiable)
-    {
-        return ['mail'];
-    }
+	/**
+	 * Create a new notification instance.
+	 *
+	 * @param string $title
+	 * @param string $message
+	 */
+	public function __construct(string $title, string $message) {
+		$this->title = $title;
+		$this->message = $message;
+	}
 
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
-    public function toMail($notifiable)
-    {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
-    }
+	/**
+	 * Get the notification's delivery channels.
+	 *
+	 * @param  mixed $notifiable
+	 * @return array
+	 */
+	public function via($notifiable) {
+		return ['database'];
+	}
 
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
-    public function toArray($notifiable)
-    {
-        return [
-            //
-        ];
-    }
+	/**
+	 * Get the mail representation of the notification.
+	 *
+	 * @param  mixed $notifiable
+	 * @return \Illuminate\Notifications\Messages\MailMessage
+	 */
+	public function toMail($notifiable) {
+		return (new MailMessage)
+			->line('The introduction to the notification.')
+			->action('Notification Action', url('/'))
+			->line('Thank you for using our application!');
+	}
+
+	/**
+	 * Get the array representation of the notification.
+	 *
+	 * @param  mixed $notifiable
+	 * @return array
+	 */
+	public function toArray($notifiable) {
+		return [
+			'title' => $this->title,
+			'message' => $this->message
+		];
+	}
 }
